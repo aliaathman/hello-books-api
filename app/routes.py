9e1aba6ca069d384848b13642ad0425fca4d2c3e
Alia Athman <aliaathmann@gmail.com>
2022-10-23 17:17:56 -0400

    created /hello-world endpoint and fixed a broken endpoint in routes file

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,17 @@
-
 from flask import Blueprint
-
-hello_world_bp = Blueprint("hello_world_bp", __name__)
-
+hello_world_bp = Blueprint("hello_world", __name__)
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def say_hello_world():
-    return "Hello, World!"
-
-
+    my_response_body = "Hello, World!"
+    return my_response_body
 @hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
+def hello_json():
     return {
-        "name": "Ada Lovelace",
+        "name": "Alia",
         "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+        "hobbies": ["Knitting", "Drawing", "Watching Reality Shows"]
     }
-
-
 @hello_world_bp.route("/broken-endpoint-with-broken-server-code")
 def broken_endpoint():
     response_body = {
@@ -28,3 +22,4 @@ def broken_endpoint():
     new_hobby = "Surfing"
     response_body["hobbies"].append(new_hobby)
     return response_body
+
