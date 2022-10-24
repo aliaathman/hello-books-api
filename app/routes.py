1105c81207f781db4bc7a308c6aadf45327f7e3b
Alia Athman <aliaathmann@gmail.com>
2022-10-23 21:28:11 -0400

    Adds the /<book_id> endpoints to allow the api to read a specific book's information if it matches the id in the url path

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 hello_world_bp = Blueprint("hello_world", __name__)
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
@@ -44,4 +44,22 @@ def handle_books():
             description = book.description
         ))
     return jsonify(books_response)
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+    
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
 
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+    for book in books:
+        if book.id == book_id:
+            return book
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
